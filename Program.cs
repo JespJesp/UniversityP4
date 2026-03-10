946818463d9e7eb4b7cffa198889770c64fcd560
Jesp <87063030+JespJesp@users.noreply.github.com>
2026-03-10 11:27:59 +0100

    De-cluttered Program.cs

--- Program.cs
+++ Program.cs
@@ -7,37 +7,17 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		string filePath = GetFilePath(args);
-		string fileContent = GetFileContent(filePath);
-		InterpretText(fileContent);
-	}
-
-	static string GetFilePath(string[] args)
-	{
 		if (args.Length == 0)
 		{
-			throw new Exception("Error getting file path from program argument: No file path provided.");
+			throw new Exception("Error in program argument: No file path provided to be interpreted.");
 		}
-		return args[0];
+
+		string filePath = args[0];
+		string fileText = File.ReadAllText(filePath);
+		InterpretText(fileText);
 	}
 
-	static string GetFileContent(string filePath)
-	{
-		try
-		{
-			return File.ReadAllText(filePath);
-		}
-		catch (FileNotFoundException)
-		{
-			throw new Exception($"Error reading file: File '{filePath}' not found.");
-		}
-		catch (Exception exception)
-		{
-			throw new Exception($"Error reading file: {exception.Message}");
-		}
-	}
-
-	static void InterpretText(string text)
+	private static void InterpretText(string text)
 	{
 		try
 		{
