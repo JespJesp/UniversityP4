d6ab146c4148007093914e05961a2cc3e5b12231
Jesp <87063030+JespJesp@users.noreply.github.com>
2026-04-30 10:21:07 +0200

    Fixed my erronious update from main.

--- Code/Ast/Nodes/Timelines/Commands/CommandNode.cs
+++ Code/Ast/Nodes/Timelines/Commands/CommandNode.cs
@@ -75,12 +75,6 @@ public class CommandNode : SymbolNode
 
 	public override void AfterSymbolUpsert(Annotator annotator)
 	{
-		// Add start commands with an identifier to the symbol table
-		if (Enum.Parse<TimelineCommandType>(CommandType, ignoreCase: true) == TimelineCommandType.Start && CommandId != "")
-		{
-			SymbolTable.Upsert(this, CommandId);
-		}
-
 		foreach (string targetId in _commandTargetIds)
 		{
 			if (targetId != "EVERYTHING"
