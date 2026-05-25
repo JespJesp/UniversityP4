0f75d3150a53034748c4d10a4a305d0af55cc87d
Chriller <cmunch24@student.aau.dk>
2026-05-25 04:20:35 +0200

    ...

--- UniversityP4.Tests/EndToEndTests/EvaluatorErrorTests.cs
+++ UniversityP4.Tests/EndToEndTests/EvaluatorErrorTests.cs
@@ -28,6 +28,16 @@ public class EvaluatorErrorTests
         yield return new object[] { EndToEndTestSupport.ProgramPath("exceeds_one_hour.mude"), "Audio duration exceeds 1 hour limit" };
     }
 
+    public static IEnumerable<object[]> ProgramsThatFailAtLexingPhase()
+    {
+        yield return new object[] { EndToEndTestSupport.ProgramPath("invalid_lexing_error.mude"), "Unterminated string (lexing error)" };
+    }
+
+    public static IEnumerable<object[]> ProgramsThatFailAtAnnotationPhase()
+    {
+        yield return new object[] { EndToEndTestSupport.ProgramPath("invalid_annotation_error.mude"), "Reference to sample before declaration (annotation error)" };
+    }
+
     [Theory]
     [MemberData(nameof(ProgramsThatFailAtParsePhase))]
     public void Program_With_Parse_Error_Should_Fail(string filePath, string errorDescription)
@@ -65,6 +75,38 @@ public class EvaluatorErrorTests
     public void Program_With_Evaluation_Error_Should_Fail(string filePath, string errorDescription)
     {
         var outputFile = EndToEndTestSupport.CreateOutputFile("e2e_evaluation_error.wav");
+
+        try
+        {
+            Should.Throw<Exception>(() => EndToEndTestSupport.RunFullPipeline(filePath, outputFile));
+        }
+        finally
+        {
+            EndToEndTestSupport.DeleteIfExists(outputFile);
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(ProgramsThatFailAtLexingPhase))]
+    public void Program_With_Lexing_Error_Should_Fail(string filePath, string errorDescription)
+    {
+        var outputFile = EndToEndTestSupport.CreateOutputFile("e2e_lexing_error.wav");
+
+        try
+        {
+            Should.Throw<Exception>(() => EndToEndTestSupport.RunFullPipeline(filePath, outputFile));
+        }
+        finally
+        {
+            EndToEndTestSupport.DeleteIfExists(outputFile);
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(ProgramsThatFailAtAnnotationPhase))]
+    public void Program_With_Annotation_Error_Should_Fail(string filePath, string errorDescription)
+    {
+        var outputFile = EndToEndTestSupport.CreateOutputFile("e2e_annotation_error.wav");
 
         try
         {
